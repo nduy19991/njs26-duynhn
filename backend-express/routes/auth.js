c86452a7f480ab8665a2339ffafa05153481ef6a
Nguyễn [H.N] Duy <rn26-duynhn>
2023-01-16 14:58:59 +0700

    DuyDepTrai

--- backend-express/routes/auth.js
+++ backend-express/routes/auth.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const yup = require('yup');
+var {validateSchema} = require('../validations/validateSchema');
 
-router.post('login', function(req, res, next) {
-    const { account, password} = req.body;
+const loginSchema =yup.object({
+    body: yup.object({
+        email: yup.string().email('Nhap dung dinh dang email').required('Vui lòng nhap email'),
+        password: yup.string().min(3, 'Nhap toi thieu 3 ky tu').max(31, 'Da vuot qua so ky tu cho phep').required('Vui lòng nhap mat khau'),
+    }),
+    params: yup.object({}),
+});
 
-    if (account === 'admin' && password === 'admin') {
-    res.send({ ok: true});
+router.post('/login', validateSchema(loginSchema), function(req, res, next) {
+    const { email, password} = req.body;
+
+    if (email === 'nduy19991@gmail.com' && password === 'nduy1991') {
+    res.send({ message: 'Dang nhap thanh cong' });
     }
-    res.status(401).send({ ok: false})
+    res.status(401).send({ message: 'Sai tai khoan hoac mat khau' })
 });
 module.exports = router
